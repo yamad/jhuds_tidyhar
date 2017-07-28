25ff2c12c2a0f52450ad30b4a9bf4b2c5da8e364
Jason Yamada-Hanff <jyamada1@gmail.com>
2017-07-27 17:24:15 -0700

    streamline code structure so data flow is more obvious

--- run_analysis.R
+++ run_analysis.R
@@ -15,62 +15,46 @@ library(tibble)
 
 ## Create combined data frame from training and test data.
 ## 
-## Assumes working directory contains file and folder structure
+## Assumes working directory contains file and folder structure 
 ## from the dataset zip file, as downloaded by `download_data.R`.
 ## This is specified behavior from assignment instructions.
 ##
 ## note: wrapping function avoids polluting global namespace with temporary variables
+##
 makeFullDataset <- function() {
     
-    features <- read.table("features.txt", col.names=c("col.id", "name"))
-    features$col.id <- features$col.id + 2  # first two cols aren't listed in features table
-    features <- features %>%                # get only mean and stddev stats
-        filter(grepl("(mean|std)\\(", name))  #   trailing parens excludes `meanFreq` features
-    
-    ## prepare dataset of type "train" or "test"
-    ##
-    ## these processing steps are identical in training and test sets
-    prepDataset <- function(dataset.type) {
-        # names
-        dir <- file.path(getwd(), dataset.type)
-        df.fname <- paste("X_", dataset.type, ".txt", sep="")          # e.g. train/X_train.txt
-        activity.fname <- paste("y_", dataset.type, ".txt", sep="")    # e.g. train/y_train.txt
-        subj.fname <- paste("subject_", dataset.type, ".txt", sep="")  # e.g. train/subject_train.txt
-        
-        df <- as_tibble(read.table(file.path(dir, df.fname)))
-        # subset desired features to reduce data ASAP
-        df <- df[,features$col.id]
-        names(df) <- features$name
-        
-        # load subject and activity ids for each row in dataset
-        subj <- read.table(file.path(dir, subj.fname), col.names=c("subject"))
-        act <- read.table(file.path(dir, activity.fname), col.names=c("activity"))
-        df <- cbind(df, subj, act)
-        
-        # mark series of points as signals (implicit from repeated rows by
-        # subject and activity)
-        df <- df %>% group_by(subject, activity) %>%
-            mutate(point=row_number()) %>%
-            ungroup
-        
-        df # return prepped data.frame
-    }
-    
-    ## Combine datasets
+    ## Combine train and test datasets
     ## -----------------------------
-    # train.data <- read.table(file.path("train", "X_train.txt"))
-    # train.subj <- read.table(file.path("train", "subject_train.txt"))
-    # train.activity <- read.table(file.path("train", "y_train.txt"))
-
-    train <- prepDataset("train")
-    test <- prepDataset("test")
-    full <- bind_rows(train, test)
+    train.data <- read.table(file.path("train", "X_train.txt"))
+    train.subj <- read.table(file.path("train", "subject_train.txt"))
+    train.activity <- read.table(file.path("train", "y_train.txt"))
+    train <- cbind(train.subj, train.activity, train.data)
     
-    ## use activity names
-    ## normalize activity labels to lowercase
-    activity.labels <- read.table("activity_labels.txt")
-    activity.labels[,2] <- tolower(activity.labels[,2])
-    full$activity <- factor(full$activity, levels=activity.labels[,1], labels=activity.labels[,2])
+    test.data <- read.table(file.path("test", "X_test.txt"))
+    test.subj <- read.table(file.path("test", "subject_test.txt"))
+    test.activity <- read.table(file.path("test", "y_test.txt"))
+    test <- cbind(test.subj, test.activity, test.data)
+    
+    full <- rbind(train, test)
+    
+    ## Make variable names, preserve just mean and std statistics
+    ## --------------------------------------
+    features <- read.table("features.txt", stringsAsFactors=FALSE)
+    # make.unique handles repeated column names
+    names(full) <- c("subject", "activity", make.unique(features[,2]))
+    # trailing parens excludes `meanFreq` features
+    full <- select(full, subject, activity, contains("mean("), contains("std"))  
+    
+    # mark series of points as signals (implicit from repeated rows by
+    # subject and activity)
+    full <- full %>% 
+        group_by(subject, activity) %>%
+        mutate(point=row_number()) %>%
+        ungroup
+    
+
+    ## Tidy data into long tidy format
+    ## --------------------------------------
     
     # convert to long tidy format by collecting all measurement columns
     full <- full %>%
@@ -78,11 +62,21 @@ makeFullDataset <- function() {
         arrange(subject, activity, measure) %>%
         select(subject, activity, measure, point, value)
     
-    # create mean and std columns
+    # extract mean and std columns
     full <- full %>%
         mutate(stat=ifelse(grepl("mean", measure), "mean", "std")) %>%
         mutate(measure=gsub("-(mean|std)\\(\\)", "", measure)) %>%
         spread(stat, value)
+    
+    
+    ## Use activity names, normalized to lowercase
+    ## --------------------------------------
+    activity.labels <- read.table("activity_labels.txt")
+    full$activity <- factor(full$activity, 
+                            levels=activity.labels[,1], 
+                            labels=tolower(activity.labels[,2]))
+    
+    full
 }
 
 ## combined and tidied dataset
@@ -90,6 +84,8 @@ har_full <- makeFullDataset()
 
 ## average over points by subject, activity, and measurement variable
 har_averages <- har_full %>% 
-    group_by(subject, activity, measure) %>%
+    group_by(subject, activity, measure) %>% 
     summarize(avg=mean(mean), std=sd(mean)) %>%
     rename(mean=avg)
+
+write.table(har_averages, file="tidy_avgs.txt", row.names=FALSE)
